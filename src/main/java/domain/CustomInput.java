5e972e15457698f76e976703a4f2303a32221a80
yunty <yty9024@naver.com>
2026-09-04 19:58:23 +0900

    refactor : 클라이언트가 사용시 순서를 알 필요없게 from 정적 팩토리 메서드로 리팩토링

--- src/main/java/domain/CustomInput.java
+++ src/main/java/domain/CustomInput.java
@@ -4,38 +4,40 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CustomInput { // 구분자와 숫자를 구분한다.
-    private String delimiter = "[,:]";
-    private String userInput = "";
-    private Pattern pattern;
-    private Matcher matcher;
+    private String delimiter;
+    private String userInput;
 
-    public void set(String userInput){
-        patternMatcher(DelimiterPattern.CUSTOM_DELIMITER_PATTERN.getRegex(),userInput);
+    private CustomInput(String rawInput) {
+        set(rawInput);
+        validateInput();
+    }
+
+    public static CustomInput from(String rawInput){
+        return new CustomInput(rawInput);
+    }
+    public String getDelimiter() {
+        return delimiter;
+    }
+    public String getUserInput() {
+        return userInput;
+    }
+
+    private void set(String rawInput){
+        this.delimiter = "[,:]";
+        this.userInput = "";
+        Pattern pattern = Pattern.compile(DelimiterPattern.CUSTOM_DELIMITER_PATTERN.getRegex());
+        Matcher matcher = pattern.matcher(rawInput);
         if(matcher.matches()){
             String customDelimiter = matcher.group(1);
             delimiter += "|"+Pattern.quote(customDelimiter);
             this.userInput = matcher.group(2);
             return;
         }
-        this.userInput=userInput;
+        this.userInput=rawInput;
     }
-    public void validateInput(){
-        patternMatcher(DelimiterPattern.DELIMITER_PATTERN.getRegex().formatted(delimiter),userInput);
-        if(!matcher.matches()){
+    private void validateInput(){
+        if(!userInput.matches(DelimiterPattern.DELIMITER_PATTERN.getRegex().formatted(delimiter))){
             throw new IllegalArgumentException();
         }
-    }
-
-    private void patternMatcher(String regex, String userInput){
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(userInput);
-    }
-
-    public String getDelimiter() {
-        return delimiter;
-    }
-
-    public String getUserInput() {
-        return userInput;
     }
 }
