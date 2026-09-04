58bb4655a06feec2757e903dbcb382dafca3f646
yunty <yty9024@naver.com>
2026-09-04 19:57:23 +0900

    refactor : 인스턴스 변수였던 sum을 지역변수로 리팩토링

--- src/main/java/domain/DelimiterSum.java
+++ src/main/java/domain/DelimiterSum.java
@@ -4,8 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DelimiterSum { //구분자와 문자열을 받아 합친다.
-    private int sum = 0;
     public int sum(CustomInput input){
+        int sum = 0;
         String[] sep = input.getUserInput().split(input.getDelimiter());
         for(String s : sep){
             sum+=Integer.parseInt(s);
