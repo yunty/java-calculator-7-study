8bde4a9ba42bf94f8b20fa71444bdd33245d657d
yunty <yty9024@naver.com>
2026-09-04 14:26:03 +0900

    refactor : 구분자 객체화 후 리팩토링

--- src/main/java/domain/DelimiterSum.java
+++ src/main/java/domain/DelimiterSum.java
@@ -3,38 +3,10 @@ package domain;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DelimiterSum {
-    private final String CUSTOM_DELIMITER_PATTERN = "//(.+)\\\\n(.*)";
-    private String delimiter = "[,:]";
-    private String userInput = "";
+public class DelimiterSum { //구분자와 문자열을 받아 합친다.
     private int sum = 0;
-
-    public void validateInput(){
-        String delimiterPattern = "^\\d+(" + delimiter + "\\d+)*$";
-        Pattern pattern = Pattern.compile(delimiterPattern);
-        Matcher matcher = pattern.matcher(userInput);
-        System.out.println(delimiter + "user:" + userInput);
-        if(!matcher.matches()){
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public void set(String userInput){
-        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_PATTERN);
-        Matcher matcher = pattern.matcher(userInput);
-
-        if(matcher.matches()){
-            String customDelimiter = matcher.group(1);
-            delimiter += "|"+Pattern.quote(customDelimiter);
-            System.out.println("커스텀");
-            this.userInput = matcher.group(2);
-            return;
-        }
-        this.userInput=userInput;
-    }
-
-    public int sum(){
-        String[] sep = userInput.split(delimiter);
+    public int sum(CustomInput input){
+        String[] sep = input.getUserInput().split(input.getDelimiter());
         for(String s : sep){
             sum+=Integer.parseInt(s);
         }
